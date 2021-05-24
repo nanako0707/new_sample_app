c4b075f58078b94e07252aefd0382630723ff42b
Nanako Kan <n.k.0213.0529@gmail.com>
2021-05-25 08:53:43 +0900

    Finish ch13 except ch13.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
+
   attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -45,6 +47,10 @@ class User < ApplicationRecord
   # 有効化用のメールを送信する
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def feed
+    Micropost.where("user_id = ?", self.id)
   end
 
   private
